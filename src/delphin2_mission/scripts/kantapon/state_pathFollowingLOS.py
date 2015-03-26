94943d483429c5de83809b8b4f18b47ccc6f266a
TanakitkornK <kt1e11@soton.ac.uk>
2015-03-26 18:44:55 +0000

    code up the LOS path following state: not yet ready

--- src/delphin2_mission/scripts/kantapon/state_pathFollowingLOS.py
+++ src/delphin2_mission/scripts/kantapon/state_pathFollowingLOS.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-# import roslib; roslib.load_manifest('delphin2_mission')
 import rospy
 import numpy
 import smach
@@ -21,6 +20,8 @@ class pathFollowingLOS(smach.State):
         self.__L_los = L_los
         self.__uGain = uGain
         self.__uMax = uMax
+        self.__wp_R = wp_R
+        self.__errHeadingTol = errHeadingTol
 
     def execute(self, userdata):
         
@@ -28,6 +29,56 @@ class pathFollowingLOS(smach.State):
         ### Perform actions ################################################
         ####################################################################
         
+        X = self.__controller.getX()
+        Y = self.__controller.getY()
+        eta = [X,Y]
+        
+        # identify the waypoint to follow
+        wpTarget = self.__uti.findFirstSegment(self.__path,eta)
+        
         # move back and forth between two waypoint with different demandProp
         while not rospy.is_shutdown():
-            pass
+            
+            X = self.__controller.getX()
+            Y = self.__controller.getY()
+            eta = [X,Y] # state vector denoted following Fossen's convention
+            
+            # waypoint switching criteria
+            if (self.__uti.waypointSwitching(path[:,wpTarget],eta,wp_R)):
+                if wpTarget == pathLen-1:
+                    # if arrive to the last waypoint, terminate the mission
+                    print 'arrived to the destination completed'
+                    print eta
+                    return 'secceeded'
+                else:
+                    # if reached the current waypoint, move onto the next line segment
+                    wpTarget += 1
+                    print 'move onto the line segment number ', wpTarget
+
+            # compute line-of-sight parameters
+            t,p_inter = self.__uti.interPointLine(path[:,wpTarget-1],path[:,wpTarget],eta)
+            vecCross = p_inter-eta # cross track error
+            ye = sqrt( vecCross[0]**2 + vecCross[1]**2 )
+            if ye>=L_los:
+                los_p = p_inter
+            else:
+                xe = sqrt( self.__L_los**2 - ye**2 ) # compute lookahead distance
+                vecAlong = path[:,wpTarget]-path[:,wpTarget-1]
+                vecAlongLen = sqrt( vecAlong[0]**2 + vecAlong[1]**2 )
+                los_p = p_inter + xe*vecAlong/vecAlongLen
+
+            los_vec = los_p-eta
+            los_a = atan2(los_vec[0],los_vec[1])*180/pi # TODO: incorporated side slip angle
+            if los_a<0: # confine the los_a within [0,2pi)
+                los_a = los_a+360
+
+            # determine heading error
+            errHeading = self.__uti.computeHeadingError(los_a,heading)
+            u = uMax*exp(-uGain*abs(errHeading))
+            
+            
+            # only publish the heading demand when the error is greater than tolerance
+            if errHeading>self.__errHeadingTol:
+                self._controller.setHeading(los_a)
+            # turn speedDemand into propeller demand and send
+            self._controller.setRearProp(round(u*22.))
