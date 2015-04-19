f0ac7173aed3777e07948d2a8c7c6a354ba7081a
smaria <sms4g13@soton.ac.uk>
2015-04-19 03:41:35 +0100

    move main into function

--- src/xsens_driver/scripts/mtdevice.py
+++ src/xsens_driver/scripts/mtdevice.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
-# creates an XSensDriver object
-# 
 import rospy
 from xsens_driver.mtdevice import XSensDriver
-		
-if __name__=='__main__':
+
+def main():
+	'''Create a ROS node and instantiate the class.'''
 
 	rospy.init_node('xsens_driver')
 	driver = XSensDriver()
-	
 	rospy.loginfo("xsens_driver online")
 	driver.spin()
+
+if __name__== '__main__':
+	main()
+	
+		
