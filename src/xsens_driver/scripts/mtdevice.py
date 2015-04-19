b038f82a78335db0b7c17aa468ece6ccbc655998
smaria <sms4g13@soton.ac.uk>
2015-04-19 02:57:54 +0100

    add missing imports

--- src/xsens_driver/scripts/mtdevice.py
+++ src/xsens_driver/scripts/mtdevice.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
+# creates an XSensDriver object
+# 
 import rospy
-
+from xsens_driver.mtdevice import XSensDriver
 		
 if __name__=='__main__':
+
 	rospy.init_node('xsens_driver')
 	driver = XSensDriver()
 	
--- src/xsens_driver/scripts/mtnode.py
+++ src/xsens_driver/scripts/mtnode.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import rospy
+from xsens_driver.mtnode import XSensDriver
 
 def main():
 	'''Create a ROS node and instantiate the class.'''
+
 	rospy.init_node('xsens_driver')
 	driver = XSensDriver()
 	driver.spin()
