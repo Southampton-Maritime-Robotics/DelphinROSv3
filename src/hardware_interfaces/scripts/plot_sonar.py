2a9958936009d5df4711d7d5693522b16448ec89
smaria <sms4g13@soton.ac.uk>
2015-05-26 13:51:42 +0100

    switch to using class instead of global

--- src/hardware_interfaces/scripts/plot_sonar.py
+++ src/hardware_interfaces/scripts/plot_sonar.py
@@ -19,10 +19,9 @@ from hardware_interfaces.msg import sonar
 ################################################################
 class plotter:
     def __init__(self):
-        self.data = [0]
+        self.data = []
         self.num_bins = 50
         #fig, az = okst.subplots()
-        self.out_queue
 
 
         #self.ani = animation.FuncAnimation(self.data
@@ -35,17 +34,26 @@ class plotter:
  
         plt.draw()
         print("%%%%%%%%%")
- 
+
+    def time_plot(self):
+        while not rospy.is_shutdown():
+            plotValues = np.swapaxes(sonar.data,0,1)
+            imshow(plotValues, origin='lower', cmap=get_cmap('gist_heat'), vmin = 0, vmax = 200, interpolation = 'none')
+
+            #plot(testvariable)
+            ion()    # for some reason all of these three are necessary
+            draw()
+            show()
+
+
 
 def get_sonar(msgData):
     # initialise figure
 
-    global testvariable
     rawData= msgData.data
     msgData = numpy.fromstring(msgData.data, dtype=numpy.uint8)
     print(msgData)
-    figure.data = msgData
-    testvariable.append(msgData)
+    sonar.data.append(msgData)
 
 def draw_test():
     if not rospy.is_shutdown():
@@ -53,17 +61,6 @@ def draw_test():
         figure.update()
         publishPID.publish(2)
         rospy.spin()
-
-def some_function():
-    global testvariable
-    while not rospy.is_shutdown():
-        plotValues = np.swapaxes(testvariable,0,1)
-        imshow(plotValues, origin='lower', cmap=get_cmap('gist_heat'), vmin = 0, vmax = 200, interpolation = 'none')
-
-        #plot(testvariable)
-        ion()    # for some reason all of these three are necessary
-        draw()
-        show()
 
         
 
@@ -73,11 +70,10 @@ if __name__ == '__main__':
 
     # if script is main, assume its run manually for debugging
     rospy.init_node('plot_sonar', log_level=rospy.DEBUG)
-    global testvariable
-    testvariable = []
     rospy.Subscriber('sonar_output', String, get_sonar)
+    sonar = plotter()
     #draw_test = rospy.Publisher('nanana', Float32, queue_size=10)
     time.sleep(2)
-    some_function()
+    sonar.time_plot()
     #plotter.update(figure)
    
