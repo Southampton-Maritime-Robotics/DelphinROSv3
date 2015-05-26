1c3c1d24cba72e19bd22b218adf659bd6d861bf2
smaria <sms4g13@soton.ac.uk>
2015-05-26 14:51:13 +0100

    add basic sonar class
    split incoming raw messages into basic, useful information

---
+++ src/hardware_interfaces/src/hardware_interfaces/analyse_sonar.py
@@ -0,0 +1,27 @@
+"""
+Useful classes, functions for analysing sonar data
+"""
+
+class sonar:
+    def __init__(self):
+        self.raw = []           # raw string of data as read from ros publisher
+        #self.timestamp = [1]     # timestamp associated with collected messages
+        self.allData = []       # all data from collected messages, extracted as array
+        self.bins = []          # bin part of messages
+        self.header = []        # header part of messages
+        self.angle = []         # angle at which measurement was made
+
+    def add_message(self, message):
+        """
+        fill in new information for a new message
+        """
+        self.raw.append(message)
+        self.allData.append(numpy.fromstring(msg.data, dtype=numpy.uint8))
+        # now split the most recent dataset:
+        self.header.append(self.allData[-1][0:52])   # 13 byte header are read from sonar
+        self.bins.append(self.allData[-1][52:])      # the rest is bins
+        self.angle = self.header[-1][40]             # this is where some of the angle seems to be
+        # TODO smaria:
+        # better understand what is contained in the sonar data
+
+
