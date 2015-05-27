703421d7d550bb988089ba07a6548fe993ba3c95
kantapon <kt1e11@soton.ac.uk>
2015-05-28 00:59:22 +0100

    add a preempted to the state as to terminate when the backSeatErrorFlag is raised

--- src/delphin2_mission/scripts/state_actions.py
+++ src/delphin2_mission/scripts/state_actions.py
@@ -18,6 +18,7 @@ import numpy
 import smach
 import smach_ros
 import time
+from std_msgs.msg import String
 
 class actions(smach.State):
     def __init__(self, lib):
@@ -27,8 +28,10 @@ class actions(smach.State):
         self.delay_action = self.delay_thruster+1500 # let the vehicle doing those actions for a period of time (value is specified in second)
             
     def execute(self, userdata):
-        outcome = 'aborted' # set exit flag to aborted by default
-        
+
+        #Set Up Publisher for Mission Control Log
+        pubMissionLog = rospy.Publisher('MissionStrings', String)
+
         ####################################################################
         ### Perform actions ################################################
         ####################################################################
@@ -44,7 +47,13 @@ class actions(smach.State):
         # and shutdown the actuators once finished
         timeStart = time.time()
         while not rospy.is_shutdown() and time.time()-timeStart < self.delay_action:
-            pass
+            if self.__controller.getBackSeatErrorFlag() == 1:
+                str= 'state_actions preempted at time = %s' %(time.time())    
+                rospy.loginfo(str)
+                pubMissionLog.publish(str)
+                return 'preempted'
+            else:
+                pass
 #            self.__controller.setDepth(0.25) # specified depth demand in [metre]
 #            self.__controller.setPitch(0) # specified pitch demand in [degree] 
 #            self.__controller.setHeading(290)
