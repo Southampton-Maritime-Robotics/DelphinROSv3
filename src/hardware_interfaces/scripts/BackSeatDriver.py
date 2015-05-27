d033900f82f48ec7c467be0306ae8f5a19858086
kantapon <kt1e11@soton.ac.uk>
2015-05-27 02:47:34 +0100

    add a section to identify if the xsens is still active

--- src/hardware_interfaces/scripts/BackSeatDriver.py
+++ src/hardware_interfaces/scripts/BackSeatDriver.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
 
 """
-A node that keeps an eye on the critical nodes. 
+A node that keeps an eye on the critical nodes and parameters. 
 
-It will raise a terminate flag when one of the critical nodes cannot working properly or the critical parameters go beyond the threshold.
+It will raise a terminating flag when at least one of the critical nodes is not working properly or one of the critical parameters goes beyond the threshold.
 
 ######################################
 #Modifications
 30/4/12 Added total water column check and heading error check
+26/5/12 Control rate of this node by using rospy.Rate()
+27/5/12 Raise a terminating flag when the xsens quiet for longer than 10 seconds
 
 """
 
@@ -22,40 +24,57 @@ def main(controller):
     rospy.init_node('back_seat_driver')
     pub=rospy.Publisher('back_seat_flag',Int8)
     pub2 = rospy.Publisher('MissionStrings', String)
+
+    controlRate = 40. # [Hz]
+    r = rospy.Rate(controlRate)
     
     # Store Initial Time
-    time_zero = time.time()    
+    time_zero = time.time()
     
     time.sleep(15) #Allow critical systems to come online.
-    time_error=time.time() #Error for heading check
-  
+    
     # Import Limit Parameters from laumch file
     overDepth = rospy.get_param('over-depth')
-    overPitch = rospy.get_param('over-pitch')    
-    overRoll = rospy.get_param('over-roll')     
-    maxInternalTemp = rospy.get_param('max-internal-temp')  
+    overPitch = rospy.get_param('over-pitch')
+    overRoll = rospy.get_param('over-roll')
+    maxInternalTemp = rospy.get_param('max-internal-temp')
     minMotorVoltage = rospy.get_param('min-motor-voltage')
     missionTimeout = rospy.get_param('mission-timeout')
     missionTimeout = missionTimeout*60 #Mission timeout in minutes therfore convert to seconds
-    
     
     #Initialise BackSeatFlag to zero
     BackSeatFlag=0
     pub2.publish('Backseat Driver Node Is Active')
     
+    headingOld = 0. # [deg]
+    timeXsensLastPublish = time.time() # [sec]
+    timeXsensDead = 5 # [sec]
+    
     while not rospy.is_shutdown():
 
-        time.sleep(0.01)
-        time_elapsed=time.time()-time_zero
-    
+        timeRef = time.time()
+        
         #Poll System For Any Potential Errors or Status Warnings
     
+        #Identify xsens status?
+        headingNow = controller.getHeading()
+        if headingNow!=headingOld:
+            timeXsensLastPublish = time.time()
+        if time.time()-timeXsensLastPublish > timeXsensDead:
+            BackSeatFlag=1
+            str = "xsens did not response for longer than %ss" %(timeXsensDead)
+            rospy.logerr(str)
+            pub.publish(BackSeatFlag)
+            pub2.publish(str)
+            return
+        else:
+            headingOld = headingNow
+
         #Identify OverDepth?
         current_depth=controller.getDepth()
-        
-        if current_depth > overDepth: 
+        if current_depth > overDepth:
             BackSeatFlag=1
-            str = "Current depth %sm > Depth limit of %sm" %(current_depth, overDepth) 
+            str = "Current depth %sm > Depth limit of %sm" %(current_depth, overDepth)
             rospy.logerr(str)
             pub.publish(BackSeatFlag)
             pub2.publish(str)
@@ -63,10 +82,9 @@ def main(controller):
         
         #Identify OverPitch?
         current_pitch=controller.getPitch()
-        
-        if abs(current_pitch) > overPitch: 
+        if abs(current_pitch) > overPitch:
             BackSeatFlag=1
-            str = "Current pitch %sdeg > Pitch limit of %sdeg" %(current_pitch, overPitch) 
+            str = "Current pitch %sdeg > Pitch limit of %sdeg" %(current_pitch, overPitch)
             rospy.logerr(str)
             pub.publish(BackSeatFlag)
             pub2.publish(str)
@@ -74,83 +92,51 @@ def main(controller):
         
         #Identify OverRoll?
         current_roll=controller.getRoll()
-        
-        if abs(current_roll) > overRoll: 
+        if abs(current_roll) > overRoll:
             BackSeatFlag=1
-            str = "Current roll %sdeg > Roll limit of %sdeg" %(current_roll, overRoll) 
+            str = "Current roll %sdeg > Roll limit of %sdeg" %(current_roll, overRoll)
             rospy.logerr(str)
             pub.publish(BackSeatFlag)
             pub2.publish(str)
-            return        
+            return
 
         #Check Internal Pressure Vessel Temperature
         current_temperature=controller.getTemperature()
-        
-        if current_temperature>maxInternalTemp: 
+        if current_temperature>maxInternalTemp:
             BackSeatFlag=1
-            str = "Current temperature %sdeg > Temperature limit of %sdeg" %(current_temperature, maxInternalTemp) 
+            str = "Current temperature %sdeg > Temperature limit of %sdeg" %(current_temperature, maxInternalTemp)
             rospy.logerr(str)
             pub.publish(BackSeatFlag)
             pub2.publish(str)
-            return   
+            return
         
         #Check Motor Voltage
         current_voltage=controller.getVoltage()
-        
-        if current_voltage<minMotorVoltage: 
+        if current_voltage<minMotorVoltage:
             BackSeatFlag=1
-            str = "Current voltage %smV < Motor voltage limit of %smV" %(current_voltage, minMotorVoltage) 
+            str = "Current voltage %smV < Motor voltage limit of %smV" %(current_voltage, minMotorVoltage)
             rospy.logerr(str)
             pub.publish(BackSeatFlag)
             pub2.publish(str)
-            return                 
+            return
         
         #Check Mission Duration
         current_time=time.time()-time_zero
-        
-        if current_time>missionTimeout: 
+        if current_time>missionTimeout:
             BackSeatFlag=1
-            str = "Current mission time %ss > Mission time limit of %ss" %(current_time, missionTimeout) 
+            str = "Current mission time %ss > Mission time limit of %ss" %(current_time, missionTimeout)
             rospy.logerr(str)
             pub.publish(BackSeatFlag)
             pub2.publish(str)
-            return   
-        
-        #Total water column depth - Only activated 30s after start of mission
-        
-   
-        
-        
+            return
 
-       # if current_total_water_column > 1:        #if current heading error less than error magnitude
-        #    time_error_water=time.time()                       #reset error time
-
-#        if (time.time()-time_error_water)>2 and current_time>30: 
-#            BackSeatFlag=1
-#            str = "Total water depth of < 1m has been observed for 2s and mission time of %s>30s" %(current_time) 
-#            rospy.logerr(str)
-#            pub.publish(BackSeatFlag)
-#            pub2.publish(str)
-#            return       
-#        
-#        #Monitor long term heading error
-#        
-#        current_heading_error=controller.getHeadingError()
-#        error_duration=60
-#        error_magnitude=20
-#        
-#        if current_heading_error<error_magnitude:        #if current heading error less than error magnitude
-#            time_error=time.time()                       #reset error time
-#            
-#        if time.time()-time_error > error_duration:      #error duration has exceeded pre-set error_duration
-#            BackSeatFlag=1
-#            str = "Heading error has exceeded %s deg for a duration of greater than %s" %(error_magnitude, error_duration) 
-#            rospy.logerr(str)
-#            pub.publish(BackSeatFlag)
-#            pub2.publish(str)
-#            return         
-#        
-
+        timeElapse = time.time()-timeRef
+        if timeElapse < controlPeriod:
+            r.sleep()
+        else:
+            str = "BackSeatDriver rate does not meet the desired value of %.2fHz: actual control rate is %.2fHz" %(controlRate,1/timeElapse)
+            rospy.logwarn(str)
+            
 if __name__ == '__main__':
     # Define an instance of highlevelcontrollibrary to pass to all action servers
     lib = library_highlevel()
