ec4e6ef1c55d02caff845de42705dbaf2b0cda6c
smaria <sms4g13@soton.ac.uk>
2015-06-17 18:05:34 +0100

    add titles, 'sort of' axis labels

--- src/hardware_interfaces/scripts/plot_sonar.py
+++ src/hardware_interfaces/scripts/plot_sonar.py
@@ -51,16 +51,53 @@ def draw_figures_pyplot():
         pylab.show()
 
 def draw_figures():
-    # replacement for draw_figures_pyplot()
+    """
+    setup the Qt Window so it updates regularly and all settings are as wished
+    """
+
     # make sure that ctrl+c quits:
     signal.signal(signal.SIGINT, signal.SIG_DFL)
+    app = QtGui.QApplication(sys.argv)
+    win = QtGui.QWidget()
+    grid = QtGui.QGridLayout()    
+    win.setLayout(grid)
+
+    ## define all plots in the window
+    w = 20
+    # bins vs time
+    bins_vs_time = pg.ImageView()
+    grid.addWidget(bins_vs_time, 1, 1, w, w)
+
+    bins = QtGui.QLabel('Bins')
+    grid.addWidget(bins, 1, 0)
+
+    time = QtGui.QLabel('Time')
+    grid.addWidget(time, w + 1, w/2)
+
+    title_bins_vs_time = QtGui.QLabel('Time vs Bin Intensity')
+    grid.addWidget(title_bins_vs_time, 0, 1, 1, w)
 
 
-    def updateA():
-        imv2.setImage(numpy.array(sonar.bins))
+    # polar bins
+    polar_bins = pg.ImageView()
+    grid.addWidget(polar_bins, 1, w + 1, w, w)
+    grid.addWidget(QtGui.QLabel('Polar plot of recent bin intensities'), 0, w + 1, 1, w)
+
+
+
+    # minimum altitude
+    # TODO
+    # distance of minimum altitude
+    # TODO
+
+
+    def update_figures():
+        #TODO make the figure zoom/drag etc. work again
+        bins_vs_time.setImage(numpy.array(sonar.bins[-150:]))
+        polar_bins.setImage(sonar.polarImage)
 
     timer = QtCore.QTimer()
-    timer.timeout.connect(updateA)
+    timer.timeout.connect(update_figures)
     timer.start(50)
     win.show()
     QtGui.QApplication.instance().exec_()
@@ -70,17 +107,20 @@ def draw_figures():
 if __name__ == '__main__':
 
     # if script is main, assume its run manually for debugging
+    # TODO if this is only used when run manually
+    # then where does the 'sonar' variable come from normally?
+
     rospy.init_node('plot_sonar', log_level=rospy.DEBUG)
     rospy.Subscriber('sonar_output', String, get_sonar)
     sonar = analyse_sonar.sonar()
     time.sleep(1)
-    #draw_figures()
+    draw_figures()
     
     
     #plot_sonar.image_plot_test(sonar)
     #plot_sonar.scrolling_plots(sonar)
     #plot_sonar.multiple_images_plot_test(sonar)
-    plot_sonar.polar_plot_test(sonar)
+    #plot_sonar.polar_plot_test(sonar)
 
 
   
