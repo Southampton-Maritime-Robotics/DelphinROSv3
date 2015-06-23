8285804b02cfde96544b32718a8748f439092d74
smaria <sms4g13@soton.ac.uk>
2015-06-23 15:38:12 +0100

    add graphical interface, dict to look up limits

--- src/hardware_interfaces/scripts/sonar_updateparams.py
+++ src/hardware_interfaces/scripts/sonar_updateparams.py
@@ -27,16 +27,44 @@ from hardware_interfaces.msg import sonar_setting
 
 class Settings(QtGui.QWidget):
  
-    def __init__(self):
+    def __init__(self,pub):
         super(Settings, self).__init__()
         #set all default values
-        self.RLim = 95.00
-        self.LLim = 85.00
-        self.heading = 0
-        self.NBins = 100
-        self.Range = 2
+        self.pub = pub
 
-        self.initUI()
+        # values of settings that are published
+        # always numbers, will be changed
+        self.settings = {'RLim': 95.0,      
+                            'LLim': 85.0,
+                            'heading': 0,
+                            'NBins': 100,
+                            'range': 2}
+
+        # limits against which user input is checked
+        # can be reference in settingsDict or fixed value
+        # limitsDict is never changed
+        self.limits = { 'RLimMin': 'LLim',    
+                            'RLimMax': 360,
+                            'LLimMin': 0,
+                            'LLimMax': 'RLim',
+                            'headingMin':0,
+                            'headingMax':360,
+                            'NBinsMin':0,
+                            'NBinsMax':100,
+                            'rangeMin':0,
+                            'rangeMax':5
+                            }
+
+        # Text that will be displayed to explain which value is changed
+        self.text = { 'RLim': 'RLim: ',
+                      'LLim': 'LLim: ',
+                      'heading': 'heading: ',
+                      'NBins': 'NBins: ',
+                      'range': 'range: ',
+                      }
+
+        
+
 
     def initUI(self):
         self.grid = QtGui.QGridLayout()
@@ -45,21 +73,53 @@ class Settings(QtGui.QWidget):
         
         self.DefineLayout()
         self.setWindowTitle('Sonar Settings')
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.Publish)
+        self.timer.start(1000)
+    
         self.show()
 
+    def Publish(self):
+        """
+        Publish sonar settings regularly
+        """
+        self.pub.publish(RLim = self.settings['RLim'], 
+                         LLim = self.settings['LLim'], 
+                         Heading = self.settings['heading'], 
+                         NBins = self.settings['NBins'], 
+                         Range = self.settings['range'])
+
+
+
     def DefineLayout(self):
-        self.text_RLim = "The current maximum angle of the sonar head sector scan is: "
-        self.label_RLim = QtGui.QLabel(self.UpdateText(self.text_RLim, self.RLim, self.LLim, 360.0))
+        """
+        Define what is shown where in the graphical interface
+        """
+        self.label_RLim = QtGui.QLabel(self.UpdateText('RLim'))
         self.grid.addWidget(self.label_RLim, 1, 0)
 
         self.input_RLim = QtGui.QLineEdit()
         self.grid.addWidget(self.input_RLim, 1, 1)
 
-        self.grid.addWidget(QtGui.QPushButton("Update Settings"), 2, 0)
+        self.button = QtGui.QPushButton("Update Settings")
+        self.grid.addWidget(self.button, 2, 0)
+        self.button.clicked.connect(self.buttonClicked)
 
-    def UpdateText(self, text, current_value, minLimit, maxLimit):
-        # TODO make this use a class of settings
-        text = text + str(current_value) + "\n" + "Enter new value between " + str(minLimit) + " and " + str(maxLimit)
+    def buttonClicked(self):
+        try:
+            self.settings['RLim'] = float(self.input_RLim.text())
+            self.label_RLim.setText(self.UpdateText('RLim'))
+
+            # publish new values
+            self.Publish()
+
+            
+        except:
+            print("Please check your input!")
+
+    def UpdateText(self, setting):
+        text = self.text[setting] + str(self.settings[setting]) + "\n"
+        text = text + "Enter new value between " + str(self.limits[setting + 'Min']) + " and " + str(self.limits[setting + 'Max'])
         return text
         
 
@@ -69,7 +129,8 @@ def sonarSettingTalker():
     rate = rospy.Rate(10) # 10hz
 
     app = QtGui.QApplication([])
-    settings = Settings()
+    settings = Settings(pub)
+    settings.initUI()   # use graphical interface
     sys.exit(app.exec_())
    
 if __name__ == '__main__':
