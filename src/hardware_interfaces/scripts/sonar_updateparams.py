2d8a6654bd2829e2f090841d3c6fc7f4535e0ee0
smaria <sms4g13@soton.ac.uk>
2015-06-23 09:58:20 +0100

    add first value input

--- src/hardware_interfaces/scripts/sonar_updateparams.py
+++ src/hardware_interfaces/scripts/sonar_updateparams.py
@@ -1,25 +1,76 @@
 #!/usr/bin/python
-import roslib; roslib.load_manifest('DelphinROS')
 import rospy
 import sys
 import serial
 import time
 import numpy
 from std_msgs.msg import String
+from PyQt4 import QtCore, QtGui
+
 
 # Import required data types
-from DelphinROS.msg import sonar
+from hardware_interfaces.msg import sonar
+from hardware_interfaces.msg import sonar_setting
 
-def sonarTalker():
-    print "Sending new parameters"
-    pub.publish(LLim=3200,RLim=3200)
+""" Interface for updating various sonar parameters
+"""
 
+
+"""
+    while not rospy.is_shutdown():
+        rospy.loginfo("Publishing sonar parameters %s" % rospy.get_time())
+        pub.publish(RLim, LLim, heading, NBins, Range)
+        rate.sleep()
+
+"""
+
+
+class Settings(QtGui.QWidget):
+ 
+    def __init__(self):
+        super(Settings, self).__init__()
+        #set all default values
+        self.RLim = 95.00
+        self.LLim = 85.00
+        self.heading = 0
+        self.NBins = 100
+        self.Range = 2
+
+        self.initUI()
+
+    def initUI(self):
+        self.grid = QtGui.QGridLayout()
+        self.setLayout(self.grid)
+        #self.statusbar = self.statusBar()
+        
+        self.DefineLayout()
+        self.setWindowTitle('Sonar Settings')
+        self.show()
+
+    def DefineLayout(self):
+        self.text_RLim = "The current maximum angle of the sonar head sector scan is: "
+        self.label_RLim = QtGui.QLabel(self.UpdateText(self.text_RLim, self.RLim, self.LLim, 360.0))
+        self.grid.addWidget(self.label_RLim, 1, 0)
+
+        self.input_RLim = QtGui.QLineEdit()
+        self.grid.addWidget(self.input_RLim, 1, 1)
+
+        self.grid.addWidget(QtGui.QPushButton("Update Settings"), 2, 0)
+
+    def UpdateText(self, text, current_value, minLimit, maxLimit):
+        # TODO make this use a class of settings
+        text = text + str(current_value) + "\n" + "Enter new value between " + str(minLimit) + " and " + str(maxLimit)
+        return text
+        
+
+def sonarSettingTalker():
+    pub = rospy.Publisher('sonar_updateSettings', sonar_setting, queue_size=10)
+    rospy.init_node('sonar_updateSettings', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+
+    app = QtGui.QApplication([])
+    settings = Settings()
+    sys.exit(app.exec_())
    
 if __name__ == '__main__':
-    rospy.init_node('sonar_updateParams')
-    pub = rospy.Publisher('sonar_update', sonar)
-    time.sleep(0.25)
-    #while not rospy.is_shutdown():
-    sonarTalker()
-    time.sleep(3)
-    #rospy.spin()
+    sonarSettingTalker()
