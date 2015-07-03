2195c8db795528bbd18b9d0a4173f7cf86f89dce
kantapon <kt1e11@soton.ac.uk>
2015-07-03 13:27:57 +0100

    general changes in the parameter values when testing the scripts with the dead_reckoner

--- src/delphin2_mission/scripts/dummySensorsAndDemands.py
+++ src/delphin2_mission/scripts/dummySensorsAndDemands.py
@@ -26,6 +26,8 @@ from hardware_interfaces.msg import depth
 from hardware_interfaces.msg import position
 from std_msgs.msg            import Int8
 from hardware_interfaces.msg import gps
+from hardware_interfaces.msg import tail_setpoints
+from hardware_interfaces.msg import tsl_setpoints
 
 from delphin2_mission.library_highlevel     import library_highlevel
 
@@ -41,6 +43,10 @@ def publishData():
     propDemand = 9.
     gpsInfo = gps()
     depthInfo = depth()
+    thVer = tsl_setpoints()
+    thHor = tsl_setpoints()
+    csVer = tail_setpoints()
+    csHor = tail_setpoints()
     
     controller = library_highlevel()
     
@@ -57,21 +63,35 @@ def publishData():
         comInfo.heading = heading # [deg] relative to North, +ve CW
 
         # fake propeller demand
-#        if time.time()-timeZero < 10:
-#            propDemand = 0
-#        elif time.time()-timeZero < 60:
-#            propDemand = 22
-#        elif time.time()-timeZero < 90:
-#            propDemand = 0
-        propDemand = 22
+        if time.time()-timeZero < 20:
+            propDemand = 0
+        elif time.time()-timeZero < 40:
+            propDemand = 22
+        elif time.time()-timeZero < 60:
+            propDemand = 10
+        elif time.time()-timeZero < 80:
+            propDemand = 15
+        elif time.time()-timeZero < 100:
+            propDemand = 0
+####        propDemand = 22
+
+        thVer.thruster0 = 0 # [2500,2500]
+        thVer.thruster1 = 0 # [2500,2500]
+        thHor.thruster0 = 0 # [2500,2500]
+        thHor.thruster1 = 0 # [2500,2500]
+
+        csVer.cs0 = 0 # [-30,30]
+        csVer.cs1 = 0 # [-30,30]
+        csHor.cs0 = 0 # [-30,30]
+        csHor.cs1 = 0 # [-30,30]
         
         # fake gps information
         gpsInfo.number_of_satelites = 5
-#        gpsInfo.fix = 0
-        if time.time()-timeZero < 70:
-            gpsInfo.fix = 1
-        elif time.time()-timeZero < 80:
-            gpsInfo.fix = 0
+        gpsInfo.fix = 0
+####        if time.time()-timeZero < 70:
+####            gpsInfo.fix = 1
+####        elif time.time()-timeZero < 80:
+####            gpsInfo.fix = 0
         gpsInfo.speed = 0.657 # [m/s]
         gpsInfo.x = 0.
         gpsInfo.y = 0.
@@ -86,7 +106,11 @@ def publishData():
         pubCompass.publish(comInfo)
         pubProp.publish(propDemand)
         pubGPS.publish(gpsInfo)
-        
+        pub_tail_ver.publish(csVer)
+        pub_tail_hor.publish(csHor)
+        pub_tsl_ver.publish(thVer)
+        pub_tsl_hor.publish(thHor)
+                
         timeElapse = time.time()-timeRef
         if timeElapse < controlPeriod:
             r.sleep()
@@ -105,6 +129,10 @@ if __name__ == '__main__':
     
     pubCompass = rospy.Publisher('compass_out', compass)
     pubProp = rospy.Publisher('prop_demand', Int8)
+    pub_tail_ver = rospy.Publisher('tail_setpoints_vertical', tail_setpoints)
+    pub_tail_hor = rospy.Publisher('tail_setpoints_horizontal', tail_setpoints)
+    pub_tsl_hor = rospy.Publisher('TSL_setpoints_horizontal', tsl_setpoints)
+    pub_tsl_ver = rospy.Publisher('TSL_setpoints_vertical', tsl_setpoints)
     pubGPS = rospy.Publisher('gps_out', gps)
     pubDepth = rospy.Publisher('depth_out', depth)
 
