a5f185a9437bbad7960b4c81de0942b702008545
kantapon <kt1e11@soton.ac.uk>
2015-10-19 23:27:57 +0100

    make a node more general and suitable to a watchdog in each actuator interface

--- src/delphin2_mission/scripts/state_goToHeading.py
+++ src/delphin2_mission/scripts/state_goToHeading.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
 '''
-Get the AUV to a certain heading.
+Usage1: if stable_time is specified
+Get the AUV to a desired heading and stay steady for some many seconds.
+
+Usage1: if stable_time is -1
+Keep publishing heading demand until a timeout criteria has been reached.
 
 @return: preemped: if the backSeatErrorFlag has been raised
-@return: succeeded: if the AUV is stable at the heading for a certain period of time
+@return: succeeded: if goal has been satisfied
 @return: aborted: if the timeout criteria has been reached
-
-# TODO
-- may need to assign the depthDemand at the same time
 
 '''
 
@@ -19,59 +20,75 @@ import time
 from std_msgs.msg import String
 
 class GoToHeading(smach.State):
-    def __init__(self, lib, myUti, demandHeading, tolerance, stable_time, timeout, controlRate):
+    def __init__(self, lib, myUti, demandHeading, stable_time, timeout):
         smach.State.__init__(self, outcomes=['succeeded','aborted','preempted'])
-        self.__controller = lib
-        self.__uti = myUti
-        self.__demandHeading = demandHeading
-        self.__tolerance = tolerance
-        self.__stable_time = stable_time
-        self.__timeout = timeout
-        self.__controlRate = controlRate
+        self.__controller       = lib
+        self.__uti              = myUti
+        self.__demandHeading    = demandHeading
+        self.__tolerance        = 3                 # [deg] a band that accounts as the AUV is at a desired heading
+        self.__stable_time      = stable_time       # AUV must stay at a desired heading for this many seconds
+        self.__timeout          = timeout           # [sec] abort criteria
+        self.__controlRate      = 5                 # [Hz]
+        self.__at_heading_time  = None
 
     def execute(self,userdata):
 
         #Set Up Publisher for Mission Control Log
         pubMissionLog = rospy.Publisher('MissionStrings', String)
         
-        if self.__controlRate>0:
-            r = rospy.Rate(self.__controlRate)
+        # Set Up Loop Timing Control
+        r = rospy.Rate(self.__controlRate)
 
-        str='Entered GoToHeading State with a demandHeading = %.3f deg' %(self.__demandHeading)
+        str='Execute GoToHeading State: heading demand = %.3f deg, stable time = %s.' %(self.__demandHeading, self.__stable_time)
         pubMissionLog.publish(str)
         rospy.loginfo(str)
 
-        timeStart = time.time()
+        at_heading = False
+        self.__at_heading_time = time.time() # a reference time for heading stady
+        timeStart = time.time()              # a reference time for state timeout
+        
         while not rospy.is_shutdown() and self.__controller.getBackSeatErrorFlag() == 0 and time.time()-timeStart < self.__timeout:
         
-            heading = self.__controller.getHeading()
-            errHeading = self.__uti.computeHeadingError(self.__demandHeading,heading)
-            if abs(errHeading)>self.__tolerance:
-                timeStart = time.time()
-            if time.time()-timeStart <= self.__stable_time:
+            if stable_time != -1:
+                at_heading = check_heading()
+                
+            if not at_heading:
                 self.__controller.setHeading(self.__demandHeading)
             else:
-                # when the heading is steady, move onto the next step
-                self.__controller.setRearProp(0)
-                self.__controller.setControlSurfaceAngle(0,0,0,0) # (VerUp,HorRight,VerDown,HorLeft)
-                self.__controller.setArduinoThrusterHorizontal(0,0) # (FrontHor,RearHor)
-                str= 'goForwards succeeded at time = %s' %(time.time())
+                str= 'goToHeading succeeded at time = %s' %(time.time())
                 pubMissionLog.publish(str)
                 rospy.loginfo(str)
                 return 'succeeded'
                 
-            if self.__controlRate>0:
-                r.sleep()
+            r.sleep()
 
-        self.__controller.setArduinoThrusterHorizontal(0,0) # (FrontHor,RearHor)
-        
         if self.__controller.getBackSeatErrorFlag() == 1:
             str= 'goForwards preempted at time = %s' %(time.time())
             pubMissionLog.publish(str)
             rospy.loginfo(str)
             return 'preempted'
         else:
-            str= 'goForwards aborted at time = %s' %(time.time())
-            pubMissionLog.publish(str)
-            rospy.loginfo(str)
-            return 'aborted'
+            if stable_time == -1:
+                str= 'goToHeading succeeded at time = %s' %(time.time())
+                pub.publish(str)
+                rospy.loginfo(str)
+                return 'succeeded'
+            else:
+                str= 'goForwards aborted at time = %s' %(time.time())
+                pubMissionLog.publish(str)
+                rospy.loginfo(str)
+                return 'aborted'
+            
+    def check_heading(self):                
+            
+        heading = self.__controller.getHeading()
+        errHeading = self.__uti.computeHeadingError(self.__demandHeading,heading)
+
+        # if the AUV lost a control in heading, reset at_heading_time
+        if abs(errHeading)>self.__tolerance:
+            self.__at_heading_time = time.time()
+
+        if time.time()-self.__at_heading_time <= self.__stable_time:
+            return True
+        else:
+            return False
