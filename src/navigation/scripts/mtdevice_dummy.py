6b270d6ee4bd8965106e056753d06ac400b7a201
SENG <kt1e11@soton.ac.uk>
2016-04-03 13:16:29 +0100

    added a node to works as a dummy xsens sensor, it helps to update AUV heading using mathematical model

---
+++ src/navigation/scripts/mtdevice_dummy.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+"""
+A node to publish a face gps information. It is mainly used when testing the dead_reckoner.
+
+"""
+
+import rospy
+import time
+
+from hardware_interfaces.msg import status
+from hardware_interfaces.msg import compass
+
+##############################
+# # # XSensDriver object # # #
+##############################
+def main_loop():
+    controlRate = 20.
+    r = rospy.Rate(controlRate)
+    
+    # to control a timing for status publishing
+    timeZero_status = time.time()
+    try:
+        dt_status = rospy.get_param('status_timing')
+    except:
+        dt_status = 2.
+        
+    while not rospy.is_shutdown():
+        # to control a timing for status publishing
+        if time.time()-timeZero_status > dt_status:
+            timeZero_status = time.time()
+            pubStatus.publish(nodeID = 6, status = True)
+            
+        comOut = compass()
+        comOut.roll = 1
+        comOut.pitch = -2.0
+        comOut.heading = 3.05
+        pubCompassOut.publish(comOut)
+        r.sleep()
+        
+            
+if __name__=='__main__': 
+    time.sleep(1) #Allow System to come Online
+    rospy.init_node('xsens_driver')
+
+    pubCompassOut = rospy.Publisher('compass_out',compass)
+    pubStatus = rospy.Publisher('status', status)
+    
+    main_loop()
