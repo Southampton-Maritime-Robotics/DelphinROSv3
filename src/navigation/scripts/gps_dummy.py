6d1bd8399aeb019bb8d0bf3019cef16c80338497
SENG <kt1e11@soton.ac.uk>
2016-04-03 13:09:42 +0100

    changed a way to publish node status a a relatively lower rate, every 2 sec

--- src/navigation/scripts/gps_dummy.py
+++ src/navigation/scripts/gps_dummy.py
@@ -23,8 +23,8 @@ def listenForData(status):
     gpsOut = gps()
 
     #Initialise values to zero - important as different messages contain different values
-    X			= 	0
-    Y			= 	0
+    X = 0
+    Y = 0
 
     controlRate = 1. # Hz
     r = rospy.Rate(controlRate)
@@ -37,8 +37,21 @@ def listenForData(status):
         lat_orig = 50.9567
         long_orig = -1.36735
 
+    # to control a timing for status publishing
+    timeZero_status = time.time()
+    try:
+        dt_status = rospy.get_param('status_timing')
+    except:
+        dt_status = 2.
+        
     timeZero = time.time()
     while not rospy.is_shutdown():
+
+        # to control a timing for status publishing
+        if time.time()-timeZero_status > dt_status:
+            timeZero_status = time.time()
+            pubStatus.publish(nodeID = 4, status = True)
+            
         X = X + 0.5
         Y = Y + 0.5
         
@@ -48,7 +61,7 @@ def listenForData(status):
             gpsOut.longitude = 666 
             gpsOut.time = 99
             gpsOut.number_of_satelites = 11
-            gpsOut.fix = 0
+            gpsOut.fix = 1
             gpsOut.speed = 99
             gpsOut.x = X
             gpsOut.y = Y
@@ -69,7 +82,6 @@ def listenForData(status):
         else:
             str = "GPS rate does not meet the desired value of %.2fHz: actual control rate is %.2fHz" %(controlRate,1/timeElapse) 
             rospy.logwarn(str)
-            pubMissionLog.publish(str)
 
 ################### INITIALISING FUNCTION ################### 
 if __name__ == '__main__':
@@ -79,7 +91,7 @@ if __name__ == '__main__':
   
     #Define Publishers
     pub = rospy.Publisher('gps_out', gps)
-    time.sleep(1)
-
+    pubStatus = rospy.Publisher('status', status)
+    
     listenForData(status)                     #Main loop for receiving data
 
