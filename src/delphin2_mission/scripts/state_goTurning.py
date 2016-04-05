0c83ce5da3a1a4461fbb504566f5138df065d321
SENG <kt1e11@soton.ac.uk>
2016-04-05 14:08:29 +0100

    a new state to activate a set of actuator relevant to turning: precisely, they are horizontal thrusters and rudder

---
+++ src/delphin2_mission/scripts/state_goTurning.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+'''
+Get the AUV to turn by using either horizontal thruster or rudder, or using both set of actuators simultaneously.
+
+If the demand for a particular set of actuators is zero, the demand of this set will not be published !!!
+
+@return: preempted: if the backSeatErrorFlag has been raised
+@return: succeeded: if the timeout criteria has been reached (for other states, timeout will lead to mission aborted)
+@return: aborted: not in use
+
+'''
+
+import rospy
+import smach
+import smach_ros
+import time
+from std_msgs.msg import String
+
+class GoTurning(smach.State):
+    def __init__(self, lib, demand_th_hor, demand_cs_ver, timeout):
+        smach.State.__init__(self, outcomes=['succeeded','aborted','preempted'])
+        self.__controller           = lib
+        self.__th_hor_frt           = demand_th_hor
+        self.__th_hor_aft           = - self.__th_hor_frt
+        self.__rudder               = demand_cs_ver
+        self.__timeout              = timeout
+        self.__controlRate          = 5 # [Hz]
+            
+    def execute(self,userdata):
+        
+        #Set Up Publisher for Mission Control Log
+        pubMissionLog = rospy.Publisher('MissionStrings', String)
+
+        # Set Up Loop Timing Control
+        r = rospy.Rate(self.__controlRate)
+
+        str='Execute GoTurning State: th_hor = %s; rudder = %s; action hold %ss' %(self.__th_hor_frt, self.__rudder, self.__timeout)
+        pubMissionLog.publish(str)
+        rospy.loginfo(str)
+        
+        timeStart = time.time()
+        while not rospy.is_shutdown() and self.__controller.getBackSeatErrorFlag() == 0 and time.time()-timeStart < self.__timeout:
+            if self.__th_hor_frt != 0 or self.__th_hor_aft != 0:
+                self.__controller.setArduinoThrusterHorizontal(self.__th_hor_frt,self.__th_hor_aft) # (FrontVer,RearVer)
+            if self.__rudder != 0:
+                self.__controller.setRudderAngle(self.__rudder)
+            r.sleep()
+        else:
+            self.__controller.setArduinoThrusterHorizontal(0,0) # (FrontVer,RearVer)
+            self.__controller.setRudderAngle(0)
+            
+        if self.__controller.getBackSeatErrorFlag() == 1:
+            str= 'GoTurning preempted at time = %s' %(time.time())
+            pubMissionLog.publish(str)
+            rospy.loginfo(str)
+            return 'preempted'
+        else: # timeout means succeed in this state
+            str= 'GoTurning succeeded at time = %s' %(time.time())
+            pubMissionLog.publish(str)
+            rospy.loginfo(str)
+            return 'succeeded'
