c3091a62dd2960cac157b827d15c9503763c032c
SENG <kt1e11@soton.ac.uk>
2016-04-09 16:49:14 +0100

    added a file to import all available basic states in one go

---
+++ src/delphin2_mission/src/delphin2_mission/basic_states.py
@@ -0,0 +1,12 @@
+#=========================
+# A list of available basic states
+#-------------------------
+
+from state_actions                          import actions
+from state_initialise                       import Initialise
+from state_stop                             import Stop
+from state_goToDepth                        import GoToDepth
+from state_goToHeading                      import GoToHeading
+from state_goForwards                       import GoForwards
+from state_goTurning                        import GoTurning
+from state_pathFollowingLOS                 import pathFollowingLOS
