5f30c008640f6ee37f262b7e380d250daa199b4c
SENG <kt1e11@soton.ac.uk>
2016-04-24 02:37:07 +0100

    added more state

--- src/delphin2_mission/src/delphin2_mission/basic_states.py
+++ src/delphin2_mission/src/delphin2_mission/basic_states.py
@@ -8,6 +8,7 @@ from state_stop                             import Stop
 from state_goToDepth                        import GoToDepth
 from state_goToHeading                      import GoToHeading
 from state_goForwards                       import GoForwards
+from state_goToAltitude                     import GoToAltitude
 from state_goTurning                        import GoTurning
 from state_pathFollowingLOS                 import pathFollowingLOS
 from state_verboseLocation                  import verboseLocation
