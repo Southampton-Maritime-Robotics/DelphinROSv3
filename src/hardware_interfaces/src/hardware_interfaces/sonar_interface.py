69211cb072ff88d49d4674cb9bbc24148e6f433f
smaria <sms4g13@soton.ac.uk>
2016-05-03 11:50:00 +0100

    add sonar reset on reoccuring serial errors

--- src/hardware_interfaces/src/hardware_interfaces/sonar_interface.py
+++ src/hardware_interfaces/src/hardware_interfaces/sonar_interface.py
@@ -2,6 +2,7 @@ import numpy
 import rospy
 import time
 import serial
+import warnings
 
 """
 VERSION AS OF 2016-04-08
@@ -18,32 +19,23 @@ class SonarTritech:
         self.RLim = rospy.get_param("sonar/RLim")
         self.LLim = rospy.get_param("sonar/LLim")
         self.NBins = rospy.get_param("sonar/NBins")
-        self.ADInterval = rospy.get_param("sonar/ADInterval")
+        #self.ADInterval = rospy.get_param("sonar/ADInterval")
+        self.Range = rospy.get_param("sonar/Range")
+        self.ADInterval = int(round((((self.Range*2)/1500.0)/self.NBins)/0.000000640,0))
         self.ADSpan = rospy.get_param("sonar/ADSpan")
         self.ADLow = rospy.get_param("sonar/ADLow")
         self.IGainB1 = rospy.get_param("sonar/IGainB1")
         self.IGainB2 = rospy.get_param("sonar/IGainB2")
         self.MoTime = rospy.get_param("sonar/MoTime")
         self.Step = rospy.get_param("sonar/Step")
-        self.SerialParam = rospy.get_param("sonar/serial") # A dictionary of the various serial components
-        try:
-            self.Serial = serial.Serial(port=self.SerialParam['Port'], baudrate = self.SerialParam['Baudrate'])
-        except:
-            rospy.logerror("Sonar could not be mounted to given serial port " + str(self.SerialParam))
-        self.Serial.bytesize = serial.EIGHTBITS
-        self.Serial.stopbits = serial.STOPBITS_ONE
-        self.Serial.parity = serial.PARITY_NONE
-        rospy.logdebug("Setup serial port for sonar: " 
-                        + str(self.Serial.portstr) + '\n' 
-                        + str(self.Serial.isOpen()))
-
-
         # Alternative expressions for calculating some of the above sonar head settings
         self.Range = rospy.get_param("sonar/Range")
         self.Data = [] # char array, where the recorded sonar ping data goes
         self.updateFlag = 0
+        self.SerialParam = rospy.get_param("sonar/serial") # A dictionary of the various serial components
+        self.receiveErrorCount = 0 # sometimes the communication with the sonar gets out of sync, this is for catching that case
+        self.serial_setup()
 
-        self.update_sonar_head_setting()
 
 
         
@@ -58,10 +50,11 @@ class SonarTritech:
         message_type = 0
         while (message_type != 4) and not rospy.is_shutdown():
             mtHeadCommand = self.get_mtHeadCommand()
-            self.serial.write(mtHeadCommand)
+            print(mtHeadCommand)
+            self.Serial.write(mtHeadCommand)
             rospy.sleep(0.1)    # 0.1 second delay  (Sophia: from old sonar code - WHY?)
             rospy.loginfo("Changing sonar head settings: Waiting for mtAlive")
-            message_flag = self.read_sonar()
+            message_type = self.read_sonar()
 
         # Flush serial buffers to ensure clean operation
         self.Serial.flushInput()
@@ -82,26 +75,38 @@ class SonarTritech:
         txPulseLength = [40,0]
         rangeScale=[60,0]
         Slope = [125, 0, 125, 0]
-        HdCtrl1 = int(self.HdCtrl1, 2)
-        HdCtrl2 = int(self.HdCtrl2, 2)
+        HdCtrl1 = int(self.SerialParam['HdCtrl1'], 2)
+        HdCtrl2 = int(self.SerialParam['HdCtrl2'], 2)
         NBins = uint16_to_uint8(self.NBins)
-        MaxADBuf = uint16_to_uint8(self.MaxADBuf)
-        Lockout = uint16_to_uint8(self.Lockout)
-        MinorAxis = uint16_to_uint8(self.MinorAxis)
-        MajorAxis = uint16_to_uint8(self.MajorAxis)
-        ScanZ = [0]
-        LF = 10
+        MaxADBuf = uint16_to_uint8(self.SerialParam['MaxADBuf'])
+        Lockout = uint16_to_uint8(self.SerialParam['Lockout'])
+        MinorAxis = uint16_to_uint8(self.SerialParam['MinorAxis'])
+        ADInterval = uint16_to_uint8(self.ADInterval)
+        print(ADInterval)
+
+        ScanZ = [0, 0]
+        LF = [10]
+        LLim = self.LLim %360
+        LLim = LLim/360. * 6400
+        LLim = uint16_to_uint8(int(LLim))
+
+        RLim = self.RLim %360
+        RLim = RLim/360. * 6400
+        RLim = uint16_to_uint8(int(RLim))
+
+
+
         mtHeadCommand = ( AsciiBlock + hexLength +
-                          [ self.SID,
-                            self.DID,
+                          [ self.SerialParam['SID'],
+                            self.SerialParam['DID'],
                             count,
                             msgNum,
-                            self.MsgSeq,
-                            self.Node,
-                            self.HeadType,
+                            self.SerialParam['MsgSeq'],
+                            self.SerialParam['Node'],
+                            self.SerialParam['HeadType'],
                             HdCtrl1,
                             HdCtrl2,
-                            self.DstHead] +
+                            self.SerialParam['DstHead'] ] +
                          TXNChan +
                          TXNChan +    #??? Is this really using the same value twice, or am I misinterpreting this? (Sophia, 2016)
                          RXNChan +
@@ -115,36 +120,42 @@ class SonarTritech:
                             self.IGainB1,
                             self.IGainB2] +
                          Slope +
-                         [  self.MoTime +
+                         [  self.MoTime,
                             self.Step] +
-                         self.ADInterval +
+                         ADInterval +
                          NBins +
                          MaxADBuf +
                          Lockout +
                          MinorAxis +
-                         [MajorAxis, self.Ctl2] +
-                         ScanZ + [LF])
-        rospy.logdebug("mtHeadCommand: "+ mtHeadCommand)
+                         [self.SerialParam['MajorAxis'], 
+                          self.SerialParam['Ctl2']] +
+                         ScanZ + 
+                         LF)
+        rospy.logdebug("mtHeadCommand: "+ str(mtHeadCommand))
         try:
             mtHeadCommand = ''.join([chr(char) for char in mtHeadCommand[:]])
         except ValueError:
             rospy.logerr("Error while generating the mtHeadCommand, ValueError")
+        
+        for a in mtHeadCommand:
+            print(ord(a))
+
         return mtHeadCommand
 
 
 
     def send_ping_trigger(self):
         # assemble array of mtHeadCommand serial communication message
-        mtSendData = (  self.Serial['HeadeMtSendData'] + 
-                        self.Serial['HexLength'] +
-                        [   self.Serial['SID'],
-                            self.Serial['DID'],
-                            self.Serial['Count'],
-                            self.Serial['MsgNum'],
-                            self.Serial['MsgSeq'],
-                            self.Serial['Node'] ] +
-                        self.Serial['CurrentTime'] +
-                        [ self.Serial['LF'] ] )
+        mtSendData = (  self.SerialParam['HeaderMtSendData'] + 
+                        self.SerialParam['HexLength'] +
+                        [   self.SerialParam['SID'],
+                            self.SerialParam['DID'],
+                            self.SerialParam['Count'],
+                            self.SerialParam['MsgNum'],
+                            self.SerialParam['MsgSeq'],
+                            self.SerialParam['Node'] ] +
+                        self.SerialParam['CurrentTime'] +
+                        [ self.SerialParam['LF'] ] )
         # convert from string format to hex format
         mtSendData = ''.join([chr(char) for char in mtSendData[:]])
         # send mtSendData message to sonar
@@ -165,17 +176,20 @@ class SonarTritech:
             if startTime + self.SerialParam['ReadTimeout'] < time.time():
                 self.Serial.flushInput()
                 rospy.loginfo("Sonar serial: read header timeout")
+                self.serial_handle_error()
                 return 0
         if self.Serial.inWaiting() > 12:
             # header Data was received,
             # analyse header data and receive to remaining message length
             headerData = self.Serial.read(13)
-            headerData = numpy.fromstring(headerData, dtype=numpy.unit8)
+            headerData = numpy.fromstring(headerData, dtype=numpy.uint8)
             rospy.logdebug("Data from sonar: mtHeadData: "
                             + str(headerData))
 
             if headerData[0] == 64:
                 # every message should start with and '@' (ASCII 64)
+                # communication is still working, so receive error count is reset
+                self.receiveErrorCount = 0
                 (msgType, msgLength) = analyse_header(headerData)
                 rospy.logdebug("Message type: " + str(msgType) +
                                " \n Message length: " + str(msgLength))
@@ -185,7 +199,8 @@ class SonarTritech:
                     if startTime + self.SerialParam['ReadTimeout'] < time.time():
                         rospy.loginfo("Sonar serial: read message rimeout")
                         self.Serial.flushInput()
-                        return 00
+                        self.serial_handle_error()
+                        return 0
 
                 msgData = self.Serial.read(self.Serial.inWaiting())
                 msgData = numpy.fromstring(msgData, dtype = numpy.uint8)
@@ -222,35 +237,72 @@ class SonarTritech:
                 else:
                     rospy.logdebug("Unknown message type: " + str(msgType))
             else:
-                rospy.logerror("Sonar message does not start with the required '@'\n"
+                rospy.logerr("Sonar message does not start with the required '@'\n"
                                + "Starting with: "+ str(headerData[0]))
+                self.serial_handle_error()
 
-def read_setting_demand(self):
-            newRLim = message.RLim
-            if (newRLim != self.p['RLim']):
-                self.updateFlag = 1 
-                self.p['RLim'] = newRLim
+    def read_setting_demand(self, message):
+                newRLim = message.RLim
+                if (newRLim != self.RLim):
+                    self.updateFlag = 1 
+                    self.RLim = newRLim
 
-            newLLim = message.LLim
-            if (newLLim != self.p['LLim']):
-                self.updateFlag = 1 
-                self.p['LLim'] = newLLim
+                newLLim = message.LLim
+                if (newLLim != self.LLim):
+                    self.updateFlag = 1 
+                    self.LLim = newLLim
 
 
-            newNBins = message.NBins
-            if (newNBins != self.p['NBins']):
-                self.updateFlag = 1 
-                self.p['NBins'] = int(newNBins)
+                newNBins = message.NBins
+                if (newNBins != self.NBins):
+                    self.updateFlag = 1 
+                    self.NBins = int(newNBins)
 
-            newRange = message.Range
-            if (newRange != self.p['Range']):
-                self.updateFlag = 1 
-                self.p['Range'] = newRange
+                newRange = message.Range
+                if (newRange != self.Range):
+                    self.updateFlag = 1 
+                    self.Range = newRange
 
-def shutdown(self):
-    self.Serial.flushInput()
-    self.Serial.flushOutput()
-    self.Serial.close()
+    def serial_shutdown(self):
+        self.Serial.flushInput()
+        self.Serial.flushOutput()
+        self.Serial.close()
+
+    def serial_setup(self):
+        try:
+            self.Serial = serial.Serial(port=self.SerialParam['Port'], baudrate = self.SerialParam['Baudrate'])
+            self.Serial.bytesize = serial.EIGHTBITS
+            self.Serial.stopbits = serial.STOPBITS_ONE
+            self.Serial.parity = serial.PARITY_NONE
+            warnings.warn("Setup serial port for sonar: " 
+                            + str(self.Serial.portstr) + '\n' 
+                            + str(self.Serial.isOpen()))
+
+
+        except:
+            warnings.warn("Sonar could not be mounted to given serial port " + str(self.SerialParam['Port']))
+
+
+
+    def serial_reset(self):
+        # this function is called to restart the serial communication
+        self.serial_shutdown()
+        self.serial_setup()
+        self.update_sonar_head_setting()
+
+    
+    def serial_handle_error():
+        # Always update error count
+        self.receiveErrorCount += 1
+        # if error count is too hight, reset serial communication
+        if self.receiveErrorCount >= self.serialParam['CriticalErrorCount']:
+            rospy.logerr("Sonar receive error count too high, 
+                            resetting sonar serial communication")
+            self.serial_reset()
+
+
+
+
 
  
 
@@ -268,4 +320,4 @@ def uint16_to_uint8(input16):
     B1 = int(numpy.uint8(input16))
     B2 = (input16-B1)/256
     output = [B1, B2]
-    return output8
+    return output
