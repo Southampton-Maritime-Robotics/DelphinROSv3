3782f8cd05a450b2cb6e6bb13ff63697ee30cd9c
smaria <sms4g13@soton.ac.uk>
2017-04-26 15:40:13 +0100

    use degrees for sonar LLim and RLim everywhere

--- src/hardware_interfaces/src/hardware_interfaces/sonar_GUI.py
+++ src/hardware_interfaces/src/hardware_interfaces/sonar_GUI.py
@@ -1,15 +1,10 @@
 import rospy
 import sys
 import traceback
-import serial
-import time
-import numpy
-from std_msgs.msg import String
 from PyQt4 import QtCore, QtGui
 
 
 # Import required data types
-from hardware_interfaces.msg import sonar
 from hardware_interfaces.msg import sonar_setting
 
 
@@ -22,45 +17,43 @@ class Settings(QtGui.QWidget):
     regularly publishes the values set in the interface
     """
 
-
- 
-    def __init__(self,pub):
+    def __init__(self, pub):
         super(Settings, self).__init__()
-        #set all default values
+        # set all default values
         self.pub = pub
 
         # values of settings that are published
         # always numbers, will be changed
         self.settings = {'RLim': 95.0,      
-                            'LLim': 85.0,
-                            'heading': 0,
-                            'NBins': 100,
-                            'range': 2}
+                         'LLim': 85.0,
+                         'heading': 0,
+                         'NBins': 100,
+                         'range': 2}
 
         # limits against which user input is checked
         # can be reference in settingsDict or fixed value
         # limitsDict is never changed
-        self.limits = { 'RLimMin': 'LLim',    
-                            'RLimMax': 360,
-                            'LLimMin': 0,
-                            'LLimMax': 'RLim',
-                            'headingMin':0,
-                            'headingMax':360,
-                            'NBinsMin':0,
-                            'NBinsMax':100,
-                            'rangeMin':0,
-                            'rangeMax':5
-                            }
+        self.limits = {'RLimMin': 'LLim',
+                       'RLimMax': 359,
+                       'LLimMin': 0,
+                       'LLimMax': 'RLim',
+                       'headingMin': 0,
+                       'headingMax': 359,
+                       'NBinsMin': 0,
+                       'NBinsMax': 100,
+                       'rangeMin': 0,
+                       'rangeMax': 5
+                       }
 
         # Text that will be displayed to explain which value is changed
-        self.text = { 'RLim': 'RLim (default: 95.0): ',
-                      'LLim': 'LLim (default: 85.0): ',
-                      'heading': 'heading (default: 3200): ',
-                      'NBins': 'NBins (default: 200): ',
-                      'range': 'range (default: 20): ',
-                      }
+        self.text = {'RLim': 'RLim (default: 95.0): ',
+                     'LLim': 'LLim (default: 85.0): ',
+                     'heading': 'heading (default: 3200): ',
+                     'NBins': 'NBins (default: 200): ',
+                     'range': 'range (default: 20): ',
+                     }
 
-         # Edit lines where new values can be entered
+        # Edit lines where new values can be entered
         self.editLines = {}
         for key in self.settings:
             self.editLines[key] = ''
@@ -70,33 +63,28 @@ class Settings(QtGui.QWidget):
         for key in self.settings:
             self.QLabels[key] = QtGui.QLabel("")
 
-      
-
-
     def initUI(self):
         self.grid = QtGui.QGridLayout()
         self.setLayout(self.grid)
-        #self.statusbar = self.statusBar()
+        # self.statusbar = self.statusBar()
         
         self.DefineLayout()
         self.setWindowTitle('Sonar Settings')
         self.timer = QtCore.QTimer()
-        self.timer.timeout.connect(self.Publish)
+        self.timer.timeout.connect(self.settings_pub)
         self.timer.start(1000)
     
         self.show()
 
-    def Publish(self):
+    def settings_pub(self):
         """
         Publish sonar settings regularly
         """
-        self.pub.publish(RLim = self.settings['RLim'], 
-                         LLim = self.settings['LLim'], 
-                         Heading = self.settings['heading'], 
-                         NBins = self.settings['NBins'], 
-                         Range = self.settings['range'])
-
-
+        self.pub.publish(RLim=self.settings['RLim'],
+                         LLim=self.settings['LLim'],
+                         Heading=self.settings['heading'],
+                         NBins=self.settings['NBins'],
+                         Range=self.settings['range'])
 
     def DefineLayout(self):
         """
@@ -104,7 +92,7 @@ class Settings(QtGui.QWidget):
         """
         d = 0
         for key in self.settings:
-            d +=1
+            d += 1
             self.QLabels[key].setText(self.UpdateText(key))
             self.grid.addWidget(self.QLabels[key], d, 0)
             self.editLines[key] = QtGui.QLineEdit()
@@ -119,12 +107,12 @@ class Settings(QtGui.QWidget):
             for key in self.settings:
                 # TODO: before accepting values, checks using the limits dictionary
                 # should be done!
-                # TODO: make the lines display the current value instead of being empy
+                # TODO: make the lines display the current value instead of being empty
                 self.settings[key] = float(self.editLines[key].text())
                 self.QLabels[key].setText(self.UpdateText(key))
 
             # publish new values
-            self.Publish()
+            self.settings_pub()
 
             
         except:
@@ -133,7 +121,8 @@ class Settings(QtGui.QWidget):
 
     def UpdateText(self, setting):
         text = self.text[setting] + str(self.settings[setting]) + "\n"
-        text = text + "Enter new value between " + str(self.limits[setting + 'Min']) + " and " + str(self.limits[setting + 'Max'])
+        text = (text + "Enter new value between " + str(self.limits[setting + 'Min']) +
+               " and " + str(self.limits[setting + 'Max']))
         return text
         
 
@@ -145,4 +134,3 @@ def sonarSettingTalker():
     settings = Settings(pub)
     settings.initUI()   # use graphical interface
     sys.exit(app.exec_())
- 
